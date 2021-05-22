f35512e5a23ae9806ebd8a78d50a317180f45e72
imilano <m15752836560_1@163.com>
2021-05-22 22:34:57 +0800

    Fix code to pass lint

--- handler/auth_test.go
+++ handler/auth_test.go
@@ -165,7 +165,7 @@ func TestAuth(t *testing.T) {
 }
 
 func TestPhone(t *testing.T) {
-	reg := regexp.MustCompile("^1(3\\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$")
+	reg := regexp.MustCompile(`^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])\d{8}$`)
 	if reg == nil {
 		log.Fatalln("regexp err")
 	}
